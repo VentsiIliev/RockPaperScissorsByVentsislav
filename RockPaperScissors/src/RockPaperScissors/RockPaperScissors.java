b54ec4f63ea885c64adcb7bfa2e1b65d21733b79
Ventsislav Iliev <ventsislav.iliew@gmail.com>
2023-04-02 00:40:26 +0300

    Update RockPaperScissors.java

--- RockPaperScissors/src/RockPaperScissors/RockPaperScissors.java
+++ RockPaperScissors/src/RockPaperScissors/RockPaperScissors.java
@@ -4,109 +4,163 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaperScissors {
+    static Scanner scanner = new Scanner(System.in);
     public static int playerWins;
     public static int computerWins;
     public static int draws;
+    public static String answer;
+    public static boolean isException;
+    public static int rounds;
+    public static int currentRound = 1;
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        boolean isException = true;
+        isException = true;
         do {
             try {
-                // ASK USER HOW MANY ROUND TO PLAY
-                System.out.println("How many rounds would you like to play?");
-                int rounds = Integer.parseInt(scanner.nextLine());
-                for (int i = 0; i < rounds; i++) {
-                    playRockPaperScissors();
-                    // TIE BREAK
-                    if (RockPaperScissors.computerWins == RockPaperScissors.playerWins) {
-                        System.out.println("It`s a tie. Do you want to play tie break rounds? [Y][N]");
-                        String answer = scanner.nextLine().toUpperCase();
-                        if ("Y".equals(answer)) {
-                            while (computerWins == playerWins) {
-                                playRockPaperScissors();
-                            }
-                        }
-                    }
-                    isException = false;
-                }
+                askHowManyRounds();
+                isException = false;
             } catch (Exception e) {
                 System.out.println("INVALID INPUT");
                 System.out.println("Please enter whole number grater than 0");
             }
-
         } while (isException);
     }
 
     public static void playRockPaperScissors() {
-        Scanner scanner = new Scanner(System.in);
 
-        // GIVE MOVE OPTIONS
+        // GIVE OPTIONS
         System.out.println("Choose wisely");
-        System.out.println("[r]ock");
-        System.out.println("[p]aper");
-        System.out.println("[s]cissors");
+        System.out.println("[R]ock, [P]aper, [S]cissors");
 
         // GAME
-        String userInput = scanner.nextLine();
+
+        // GET INPUT
         // VALIDATE INPUT
-        while (!"r".equals(userInput) && !"p".equals(userInput) && !"s".equals(userInput)) {
+        String playerInput = scanner.nextLine().toLowerCase();
+        while (!"r".equals(playerInput) && !"p".equals(playerInput) && !"s".equals(playerInput)) {
             System.out.println("Invalid input.");
-            System.out.println("Please enter a valid number ([r]ock, [p]aper, [s]cissors).");
-            userInput = scanner.nextLine();
-        }
-        // PLAYER MOVE
-        String userChoice = "";
-        if ("r".equals(userInput)) {
-            userChoice = "Rock";
-        } else if ("p".equals(userInput)) {
-            userChoice = "Paper";
-        } else {
-            userChoice = "Scissors";
+            System.out.println("[R]ock, [P]aper, [S]cissors");
+            playerInput = scanner.nextLine().toLowerCase();
         }
 
-        System.out.printf("You chose %s!\n", userChoice);
-        // COMPUTER MOVE
+
+        // PLAYER CHOISE
+        String playerChoice = "";
+        if ("r".equals(playerInput)) {
+            playerChoice = "Rock";
+        } else if ("p".equals(playerInput)) {
+            playerChoice = "Paper";
+        } else {
+            playerChoice = "Scissors";
+        }
+        System.out.printf("--------- ROUND: %d ---------\n", currentRound);
+        System.out.printf("You chose %s!\n", playerChoice);
+
+
+        // COMPUTER CHOICE
         Random random = new Random();
         int randomNumber = random.nextInt(3);
 
-        String computerMove = "";
+        String computerChoice = "";
         if (randomNumber == 0) {
-            computerMove = "Rock";
+            computerChoice = "Rock";
         } else if (randomNumber == 1) {
-            computerMove = "Paper";
+            computerChoice = "Paper";
         } else {
-            computerMove = "Scissors";
+            computerChoice = "Scissors";
         }
+        System.out.printf("The computer chose %s!\n", computerChoice);
 
-        System.out.printf("The computer chose %s!\n", computerMove);
-        // ROUND OUTCOME
-        if ("r".equals(userInput) && randomNumber == 2) {
-            RockPaperScissors.playerWins++;
+
+        // ROUND OUTCOME OPTIONS
+        if ("r".equals(playerInput) && randomNumber == 2) {
+            playerWins++;
             System.out.println("YOU WIN!!");
-        } else if ("p".equals(userInput) && randomNumber == 0) {
-            RockPaperScissors.playerWins++;
+        } else if ("p".equals(playerInput) && randomNumber == 0) {
+            playerWins++;
             System.out.println("YOU WIN!");
-        } else if ("s".equals(userInput) && randomNumber == 1) {
-            RockPaperScissors.playerWins++;
+        } else if ("s".equals(playerInput) && randomNumber == 1) {
+            playerWins++;
             System.out.println("YOU WIN!");
-        } else if (userChoice.equals(computerMove)) {
-            RockPaperScissors.draws++;
+        } else if (playerChoice.equals(computerChoice)) {
+            draws++;
             System.out.println("It`s a draw");
         } else {
-            RockPaperScissors.computerWins++;
+            computerWins++;
             System.out.println("YOU LOST!");
         }
-        System.out.println("************************");
+        System.out.println("----------------------------");
+
 
         // STATS
         System.out.println("Stats");
-        System.out.printf("You %d : %d Computer\n", RockPaperScissors.playerWins, RockPaperScissors.computerWins);
+        System.out.printf("You %d : %d Computer\n", playerWins, computerWins);
 
-        if (RockPaperScissors.draws > 0) {
-            System.out.printf("Draws: %d\n", RockPaperScissors.draws);
+        if (draws > 0) {
+            System.out.printf("Draws: %d\n", draws);
+        }
+        // TIE BREAK
+        // VALIDATE INPUT
+        if (currentRound == rounds) {
+            if (computerWins == playerWins) {
+                System.out.println("It`s a tie. Do you want to play tie break rounds? [Y]es [N]o");
+                answer = scanner.nextLine().toUpperCase();
+                while (!"Y".equals(answer) && !"N".equals(answer)) {
+                    System.out.println("INVALID INPUT");
+                    System.out.println("[Y]es [N]o");
+                    answer = scanner.nextLine().toUpperCase();
+                }
+                if ("Y".equals(answer)) {
+                    while (computerWins == playerWins) {
+                        playRockPaperScissors();
+                    }
+                } else {
+                    askIfPlayerWantToPlayAgain();
+                }
+            }
+        }
+        askIfPlayerWantToPlayAgain();
+    }
+
+    // ASK PLAYER HOW MANY ROUND TO PLAY
+    // VALIDATE INPUT
+    public static void askHowManyRounds() {
+        System.out.println("How many rounds would you like to play?");
+        rounds = Integer.parseInt(scanner.nextLine());
+        while (rounds <= 0) {
+            System.out.println("Please enter whole number grater than 0!");
+            rounds = Integer.parseInt(scanner.nextLine());
+        }
+        if (rounds == 1) {
+            playRockPaperScissors();
+        } else {
+            for (int i = 1; i <= rounds; i++) {
+                playRockPaperScissors();
+                currentRound++;
+            }
+        }
+    }
+
+    public static void askIfPlayerWantToPlayAgain() {
+        // ASK IF PLAYER WANTS TO PLAY AGAIN
+        if (currentRound == rounds) {
+            System.out.println("Do you want to start a new game? [Y]es [N]o");
+            answer = scanner.nextLine().toUpperCase();
+            while (!"Y".equals(answer) && !"N".equals(answer)) {
+                System.out.println("INVALID INPUT");
+                System.out.println("[Y]es [N]o");
+                answer = scanner.nextLine().toUpperCase();
+            }
+            if ("Y".equals(answer)) {
+                currentRound = 1;
+                computerWins = 0;
+                playerWins = 0;
+                draws = 0;
+                askHowManyRounds();
+            } else {
+                System.exit(0);
+            }
         }
     }
 }
-
