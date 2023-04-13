486abb7f0cc39ad2dfe9bcb1abcc661f8995a12a
Ventsislav Iliev <ventsislav.iliew@gmail.com>
2023-04-13 10:39:44 +0300

    Update Main.java

--- RockPaperScissors/src/RockPaperScissors/Main.java
+++ RockPaperScissors/src/RockPaperScissors/Main.java
@@ -4,16 +4,17 @@ public class Main {
     public static void main(String[] args) {
         RockPaperScissors playGame = new RockPaperScissors();
 
-            RockPaperScissors.isException = true;
-            do {
-                try {
-                    RockPaperScissors.askHowManyRounds();
-                    RockPaperScissors.isException = false;
-                } catch (Exception e) {
-                    System.out.println("INVALID INPUT");
-                    System.out.println("Please enter whole number grater than 0");
-                }
-            } while (RockPaperScissors.isException);
+        RockPaperScissors.isException = true;
+        do {
+            try {
+                RockPaperScissors.askHowManyRounds();
+                RockPaperScissors.startGame();
+                RockPaperScissors.isException = false;
+            } catch (Exception e) {
+                System.out.println("INVALID INPUT");
+                System.out.println("Please enter whole number grater than 0");
+            }
+        } while (RockPaperScissors.isException);
 
     }
 }
